fe3e9f94d66ae4e58b265f4983a30eb7f8585e8d
SourSushi360 <jplopez365@hotmail.com>
2024-04-09 13:21:00 -0600

    updated css

--- poke-proyect/src/App.jsx
+++ poke-proyect/src/App.jsx
@@ -13,17 +13,24 @@ function App() {
         <div className='screen-box'>
           <div className='screen'></div>
         </div>
-
         <div className='button-container' id='container'>
-          <div className='container-pad' id='container'></div>
+          <div className='container-pad' id='container'>
+            <div className='pad'id='up'></div>
+            <div id='middle'>
+              <div className='pad' id='left'></div>
+              <div className='pad' id='center'></div>
+              <div className='pad' id='right'></div>
+            </div>
+            <div className='pad'id='down'></div>
+          </div>
           <div className='container-select' id='container'>
             <div className='container' id='first'>
               <div className='button-select' id='button'></div>
-              <div>select</div>
+              <div>SELECT</div>
             </div>
             <div className='container'>
               <div className='button-start' id='button'></div>
-              <div>start</div>
+              <div>START</div>
             </div>
           </div>
           <div className='container-action' id='container'>
@@ -38,6 +45,18 @@ function App() {
               </div>
             </div>
           </div>
+        </div>
+        <div id='decor'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <span></span>
+        </div>
+        <div className='label'>
+          <div id='N'>Nintendo</div><div id='GB'>GAME BOY</div><div id='tm'>TM</div>
         </div>
       </div>
     </div>
